25be4ad247c8d90feae9fe6ed1f0ca6c78657fd4
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-09-02 20:30:34 +0600

    feat: refactor user creation logic and move to user service

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import { User } from "./user.model";
+import { UserServices } from "./user.service";
 
+
+// create a user
 const createUser = async (req: Request, res: Response) => {
   try {
-    const { name, email } = req.body;
 
-    const user = await User.create({ name, email });
+    // handle createUser from user.service file 
+    const user = await UserServices.createUser(req.body);
 
     res.status(StatusCodes.CREATED).json({
       messsage: "User created successfully!",
@@ -28,3 +30,15 @@ const createUser = async (req: Request, res: Response) => {
 export const UserControllers = {
   createUser
 }
+
+
+// Steps:
+// route matching -> UserRoutes -> UserController.createUser -> UserServices.createUser
+
+
+// Steps
+// 1. user.interface.ts -> create user Shape
+// 2. user.model.ts -> create user model
+// 3. user.route.ts -> here handle all routes like POST, GET, PUT, PATCH, DELETE
+// 4. user.controller.ts -> 
+// 5. user.services.ts 
