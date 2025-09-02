4369776c7e9d7f3f371798dded26ca8e0420077a
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-09-02 16:00:10 +0600

    feat: create UserRoutes add morgan for logging and restructure routes

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,23 @@
-
 import cors from "cors";
 import express, { Application, Request, Response } from 'express';
-import { UserRoutes } from './app/modules/User/user.route';
+import morgan from "morgan";
+import { router } from "./app/routes";
+
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(morgan("dev"));
 
 
 // handle user rotues
-app.use("/api/v1/user", UserRoutes )
+app.use("/api/v1", router);
 
 
-app.get("/", (req : Request, res:Response) => {
-  res.json({message:"Welcome to Tour management Systen backend Server!!"})
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Welcome to Tour management Systen backend Server!!" });
 });
-
 
 export default app;
