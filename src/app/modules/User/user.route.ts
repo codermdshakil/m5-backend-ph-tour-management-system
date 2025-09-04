c73b0f38dfb85fda6ea7fabf02c3239c05b202b6
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-09-04 12:35:26 +0600

    feat: add user validation schema using Zod for registration

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -1,10 +1,22 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { UserControllers } from "./user.controller";
 
 const router = Router();
+const passwordRegex =
+  /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+={}[\]|:;"'<>,.?/~`]).{8,}$/;
 
-router.post("/register", UserControllers.createUser);
+router.post(
+  "/register",
+  async (req: Request, res: Response, next: NextFunction) => {
+   
+    req.body = await createUserZodSchema.parseAsync(req.body);
+    console.log(req.body, 'from hit');
+
+    // next();
+  },
+  UserControllers.createUser
+);
+
 router.get("/all-users", UserControllers.getAllUsers);
-
 
 export const UserRoutes = router;
