d1fe34bbff436abe524fe7ec2cc574ac61607dac
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-09-05 23:11:07 +0600

    update: on checkAuth here sent roles from Role interface

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -2,13 +2,14 @@ import { Router } from "express";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { UserControllers } from "./user.controller";
+import { Role } from "./user.interface";
 import { createUserZodSchema } from "./user.validation";
 
 const router = Router();
 
 
 router.post("/register", validateRequest(createUserZodSchema), UserControllers.createUser);
-router.get("/all-users",checkAuth("ADMIN", "SUPER_ADMIN"),UserControllers.getAllUsers);
+router.get("/all-users",checkAuth(Role.ADMIN, Role.SUPER_ADMIN),UserControllers.getAllUsers);
 
 
 export const UserRoutes = router;
