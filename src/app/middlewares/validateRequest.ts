1c3a0de33b0a4637338ae925a4d043ab9b722ac3
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-09-06 13:08:26 +0600

    ## Summary
    - createUserZodSchema, updateUserZodSchema
    - validateReqest
    - checkAuth - valid user can update this profile, super admin can update everything but admin can’t make himself super_admin
    - credentialLogin - using email and password JWT token based
    - updateUser - here we handle some cases that is admin can update admin and user level things but admin can’t update SUPER_ADMIN things but SUPER_ADMIN can access to change ADMIN + USER + GUIDE everthing
    - hashPassword - using bcryptjs
    - generateToken - JWT
    - verifyToken - JWT
    - seedSuperAdmin()  - if super_admin don’t exist automatically create a super_admin when server will start

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -3,9 +3,7 @@ import { ZodTypeAny } from "zod";
 
 export const validateRequest = (zodSchema: ZodTypeAny) => async (req: Request, res: Response, next: NextFunction) => {
     try {
-      console.log("old", req.body);
         req.body = await zodSchema.parseAsync(req.body)
-        console.log("new ", req.body);
         next()
     } catch (error) {
         next(error)
