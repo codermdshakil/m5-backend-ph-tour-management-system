9e7f907ac55b0d75c3bf1f5d7c1b9013946d4d01
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-09-16 11:29:49 +0600

    Part - 4 completed

--- src/app/utils/catchAsync.ts
+++ src/app/utils/catchAsync.ts
@@ -1,9 +1,19 @@
 import { NextFunction, Request, Response } from "express";
+import { envVars } from "../config/env";
 
-type AsyncHander = (req:Request, res:Response, next:NextFunction) => Promise<void>
+type AsyncHander = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<void>;
 
-export const catchAsync = (fn: AsyncHander) => (req:Request, res:Response, next:NextFunction) => {
-  Promise.resolve(fn(req, res, next)).catch((err:any) => {
-    next(err)
-  })
-}
+export const catchAsync =
+  (fn: AsyncHander) => (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch((err: any) => {
+      // check
+      if (envVars.NODE_ENV === "development") {
+        console.log(err);
+      }
+      next(err);
+    });
+  };
