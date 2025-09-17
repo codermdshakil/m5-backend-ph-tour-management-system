8f70df54ec9e56fe707a1cc235a9165aee077336
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-09-17 14:33:46 +0600

    Create: TourType Interface and Model

--- src/app/modules/Tour/tour.model.ts
+++ src/app/modules/Tour/tour.model.ts
@@ -1,16 +1,6 @@
 import { model, Schema } from "mongoose";
-import { ITour, ITourType } from "./tour.interface";
+import { ITour } from "./tour.interface";
 
-const tourTypeSchema = new Schema<ITourType>(
-  {
-    name: { type: String, required: true , unique:true},
-  },
-  {
-    timestamps: true,
-  }
-);
-
-export const TourType = model<ITourType>("TourType", tourTypeSchema)
 
 const tourSchema = new Schema<ITour>(
   {
