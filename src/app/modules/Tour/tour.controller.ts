d4863674e44f200ef456831650a15144f117f8fa
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-09-18 11:50:46 +0600

    Update methods of tour

--- src/app/modules/Tour/tour.controller.ts
+++ src/app/modules/Tour/tour.controller.ts
@@ -1,26 +1,61 @@
-import { NextFunction, Request, Response } from "express";
-import { StatusCodes } from "http-status-codes";
+import { Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import { sentResponse } from "../../utils/sendResponse";
 import { TourServices } from "./tour.services";
 
 // create
-const createTour = catchAsync(async(req:Request, res:Response, next:NextFunction) => {
-
-  const tour = await TourServices.createTour(req.body);
-
-  sentResponse(res, {
-      success: true,
-      statusCode: StatusCodes.CREATED,
-      message: "Successfully Tour Created!",
-      data: tour,
+const createTour = catchAsync(async (req: Request, res: Response) => {
+    const result = await TourServices.createTour(req.body);
+    sentResponse(res, {
+        statusCode: 201,
+        success: true,
+        message: 'Tour created successfully',
+        data: result,
     });
-
 });
 
-// 
+// get all tourss
+const getAllTours = catchAsync(async (req: Request, res: Response) => {
+
+    const query = req.query
+    const result = await TourServices.getAllTours(query as Record<string, string>);
+    sentResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: 'Tours retrieved successfully',
+        data: result.data,
+        meta: result.meta,
+    });
+});
+
+// update
+const updateTour = catchAsync(async (req: Request, res: Response) => {
+
+    const result = await TourServices.updateTour(req.params.id, req.body);
+    sentResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: 'Tour updated successfully',
+        data: result,
+    });
+});
+
+// delete
+const deleteTour = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await TourServices.deleteTour(id);
+    sentResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: 'Tour deleted successfully',
+        data: result,
+    });
+});
 
 export const TourController = {
   createTour,
+  getAllTours,
+  updateTour,
+  deleteTour
 }
 
--- src/app/modules/Tour/tour.services.ts
+++ src/app/modules/Tour/tour.services.ts
@@ -1,28 +1,71 @@
+import AppError from "../../errorHelpers/appError";
+import { QueryBuilder } from "../../utils/QueryBuilder";
+import { tourSearchableFields } from "./tour.constant";
 import { ITour } from "./tour.interface";
 import { Tour } from "./tour.model";
 
 // create
-const createTour = async (payload: Partial<ITour>) => {
-  const existingTour = await Tour.findOne({ title: payload.title });
-  if (existingTour) {
-    throw new Error("A tour with this title already exists.");
-  }
+const createTour = async (payload: ITour) => {
+    const existingTour = await Tour.findOne({ title: payload.title });
 
-  const baseSlug = (payload.title as string).toLowerCase().split(" ").join("-");
-  let slug = `${baseSlug}`;
+    if (existingTour) {
+        throw new AppError(400,"A tour with this title already exists.");
+    }
 
-  let counter = 0;
-  while (await Tour.exists({ slug })) {
-    slug = `${slug}-${counter++}`; // dhaka-division-2
-  }
+    const tour = await Tour.create(payload)
 
-  payload.slug = slug;
-
-  const tour = await Tour.create(payload);
-
-  return tour;
+    return tour;
 };
+
+// get all tours
+const getAllTours = async (query: Record<string, string>) => {
+
+
+    const queryBuilder = new QueryBuilder(Tour.find(), query)
+
+    const tours = await queryBuilder
+        .search(tourSearchableFields)
+        .filter()
+        .sort()
+        .fields()
+        .paginate()
+
+
+    const [data, meta] = await Promise.all([
+        tours.build(),
+        queryBuilder.getMeta()
+    ])
+
+
+    return {
+        data,
+        meta
+    }
+};
+
+// update
+const updateTour = async (id: string, payload: Partial<ITour>) => {
+
+    const existingTour = await Tour.findById(id);
+
+    if (!existingTour) {
+        throw new Error("Tour not found.");
+    }
+    const updatedTour = await Tour.findByIdAndUpdate(id, payload, { new: true });
+
+    return updatedTour;
+};
+
+// delete
+const deleteTour = async (id: string) => {
+    return await Tour.findByIdAndDelete(id);
+};
+
 
 export const TourServices = {
   createTour,
+  getAllTours,
+  updateTour,
+  deleteTour
+
 };
