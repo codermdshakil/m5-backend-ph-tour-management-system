93a2a1f3c23d4e7d626c3bae15cee81b1e024e83
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-10-01 16:13:43 +0600

    Create: QueryBuilder class and methods filter,search

--- src/app/modules/Tour/tour.services.ts
+++ src/app/modules/Tour/tour.services.ts
@@ -1,6 +1,5 @@
-import { StatusCodes } from "http-status-codes";
-import { excludeField } from "../../constants";
 import AppError from "../../errorHelpers/appError";
+import { QueryBuilder } from "../../utils/QueryBuilder";
 import { tourSearchableFields } from "./tour.constant";
 import { ITour } from "./tour.interface";
 import { Tour } from "./tour.model";
@@ -18,79 +17,95 @@ const createTour = async (payload: ITour) => {
   return tour;
 };
 
+
+// get all tours
+// const getAllToursOld = async (query: Record<string, string>) => {
+//   const filter = query;
+//   const searchTerm = query.searchTerm || "";
+//   const sort = query.sort || "-createdAt";
+//   const page = Number(query.page) || 1;
+//   const limit = Number(query.limit) || 10;
+//   const skip = (page - 1) * limit;
+
+//   // field filtering
+//   const fields = query.fields?.split(",").join(" ") || "";
+
+//   for (const field of excludeField) {
+//     delete filter[field];
+//   }
+
+//   // raw filtering
+//   // const tours = await Tour.find(filter);
+
+//   // raw searching
+
+//   // ##  search just based one field that is title
+//   // const tours = await Tour.find({
+//   //     title:{$regex:searchTerm, $options:"i"}
+//   // });
+
+//   // ## search just based on multiple field that is  title , description, location
+
+//   const searchQuery = {
+//     $or: tourSearchableFields.map((field) => ({
+//       [field]: { $regex: searchTerm, $options: "i" },
+//     })),
+//   };
+
+//   // calculate meta data
+//   const totalTours = await Tour.countDocuments();
+//   const totalPage = Math.ceil(totalTours / limit);
+
+//   const metaData = {
+//     page: page,
+//     limit: limit,
+//     totalPage: totalPage,
+//     totalTours: totalTours,
+//   };
+
+//   //   handle page if greater
+//   if (page > totalPage) {
+//     throw new AppError(
+//       StatusCodes.NOT_FOUND,
+//       "Page is greather then total Page!"
+//     );
+//   }
+
+//   // One way Qury method
+//   //   const tours = await Tour.find(searchQuery)
+//   //     .find(filter)
+//   //     .sort(sort)
+//   //     .select(fields)
+//   //     .skip(skip)
+//   //     .limit(limit);
+
+//   // Another way query method
+//   const filterQuery = Tour.find(filter);
+//   const tours = filterQuery.find(searchQuery);
+//   const allTours = await tours
+//     .sort(sort)
+//     .select(fields)
+//     .skip(skip)
+//     .limit(limit);
+
+//   return {
+//     data: allTours,
+//     meta: metaData,
+//   };
+// };
+
+
 // get all tours
 const getAllTours = async (query: Record<string, string>) => {
-  const filter = query;
-  const searchTerm = query.searchTerm || "";
-  const sort = query.sort || "-createdAt";
-  const page = Number(query.page) || 1;
-  const limit = Number(query.limit) || 10;
-  const skip = (page - 1) * limit;
-
-  // field filtering
-  const fields = query.fields?.split(",").join(" ") || "";
-
-  for (const field of excludeField) {
-    delete filter[field];
-  }
-
-  // raw filtering
-  // const tours = await Tour.find(filter);
-
-  // raw searching
-
-  // ##  search just based one field that is title
-  // const tours = await Tour.find({
-  //     title:{$regex:searchTerm, $options:"i"}
-  // });
-
-  // ## search just based on multiple field that is  title , description, location
-
-  const searchQuery = {
-    $or: tourSearchableFields.map((field) => ({
-      [field]: { $regex: searchTerm, $options: "i" },
-    })),
-  };
-
-  // calculate meta data
-  const totalTours = await Tour.countDocuments();
-  const totalPage = Math.ceil(totalTours / limit);
-
-  const metaData = {
-    page: page,
-    limit: limit,
-    totalPage: totalPage,
-    totalTours: totalTours,
-  };
-
-  //   handle page if greater
-  if (page > totalPage) {
-    throw new AppError(
-      StatusCodes.NOT_FOUND,
-      "Page is greather then total Page!"
-    );
-  }
-
-  // One way Qury method
-  //   const tours = await Tour.find(searchQuery)
-  //     .find(filter)
-  //     .sort(sort)
-  //     .select(fields)
-  //     .skip(skip)
-  //     .limit(limit);
-
-  // Another way query method
-  const filterQuery = Tour.find(filter);
-  const tours = filterQuery.find(searchQuery);
-  const allTours = await tours
-    .sort(sort)
-    .select(fields)
-    .skip(skip)
-    .limit(limit);
+  const queryBuilder = new QueryBuilder(Tour.find(), query);
+  // implement filter method
+  const tours = await queryBuilder.search(tourSearchableFields).filter().modelQuery;
+  
+ 
 
   return {
-    data: allTours,
-    meta: metaData,
+    data: tours,
+    // meta: metaData,
   };
 };
 
--- src/app/utils/QueryBuilder.ts
+++ src/app/utils/QueryBuilder.ts
@@ -1,76 +1,45 @@
 import { Query } from "mongoose";
 import { excludeField } from "../constants";
 
+// Build a QueryBuilder
 export class QueryBuilder<T> {
-    public modelQuery: Query<T[], T>;
-    public readonly query: Record<string, string>
+  public modelQuery: Query<T[], T>;
+  public readonly query: Record<string, string>;
 
-    constructor(modelQuery: Query<T[], T>, query: Record<string, string>) {
-        this.modelQuery = modelQuery;
-        this.query = query;
+  constructor(modelQuery: Query<T[], T>, query: Record<string, string>) {
+    this.modelQuery = modelQuery;
+    this.query = query;
+  }
+
+  // Filter methods
+  filter(): this {
+    // get filter from query
+    const filter = { ...this.query };
+
+    // remove other properties except filter
+    for (const field of excludeField) {
+      delete filter[field];
     }
+    // filter method implement
+    this.modelQuery = this.modelQuery.find(filter);
+    return this;
+  }
 
+  // search method
+  search(searchableField: string[]): this {
+    // get searchTerm
+    const searchTerm = this.query.searchTerm || "";
 
-    filter(): this {
-        const filter = { ...this.query }
+    // based on searchTerm searching on fields
+    const searchQuery = {
+      $or: searchableField.map((field) => ({
+        [field]: { $regex: searchTerm, $options: "i" },
+      })),
+    };
 
-        for (const field of excludeField) {
-            delete filter[field]
-        }
+    // search method
+    this.modelQuery = this.modelQuery.find(searchQuery);
 
-        this.modelQuery = this.modelQuery.find(filter);
-
-        return this;
-    }
-
-    search(searchableField: string[]): this {
-        const searchTerm = this.query.searchTerm || ""
-        const searchQuery = {
-            $or: searchableField.map(field => ({ [field]: { $regex: searchTerm, $options: "i" } }))
-        }
-        this.modelQuery = this.modelQuery.find(searchQuery)
-        return this
-    }
-
-    sort(): this {
-
-        const sort = this.query.sort || "-createdAt";
-
-        this.modelQuery = this.modelQuery.sort(sort)
-
-        return this;
-    }
-    fields(): this {
-
-        const fields = this.query.fields?.split(",").join(" ") || ""
-
-        this.modelQuery = this.modelQuery.select(fields)
-
-        return this;
-    }
-    paginate(): this {
-
-        const page = Number(this.query.page) || 1
-        const limit = Number(this.query.limit) || 10
-        const skip = (page - 1) * limit
-
-        this.modelQuery = this.modelQuery.skip(skip).limit(limit)
-
-        return this;
-    }
-
-    build() {
-        return this.modelQuery
-    }
-
-    async getMeta() {
-        const totalDocuments = await this.modelQuery.model.countDocuments()
-
-        const page = Number(this.query.page) || 1
-        const limit = Number(this.query.limit) || 10
-
-        const totalPage = Math.ceil(totalDocuments / limit)
-
-        return { page, limit, total: totalDocuments, totalPage }
-    }
+    return this;
+  }
 }
