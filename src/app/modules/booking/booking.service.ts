cf01dea2d4af40574063f50c183253e9f15b58ef
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-10-09 11:46:22 +0600

    Implement : transaction and rollback

--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -21,47 +21,74 @@ const getTransactionId = () => {
 const createBooking = async (payload: Partial<IBooking>, userId: string) => {
   const transactionId = getTransactionId();
 
-  const user = await User.findById(userId);
+  console.log(payload,);
 
-  if (!user?.phone || !user?.address) {
-    throw new AppError(
-      StatusCodes.BAD_REQUEST,
-      "Please update your profile to book a Tour!"
+  // Implement transaction Rollback
+  // transaction rollback initialaise
+  const session = await Booking.startSession();
+  session.startTransaction();
+
+  try {
+    const user = await User.findById(userId);
+
+    if (!user?.phone || !user?.address) {
+      throw new AppError(
+        StatusCodes.BAD_REQUEST,
+        "Please update your profile to book a Tour!"
+      );
+    }
+
+    // tour
+    const tour = await Tour.findById(payload.tour).select("costFrom");
+
+    if (!tour?.costFrom) {
+      throw new AppError(StatusCodes.BAD_REQUEST, "No tour cost found!");
+    }
+
+    // calculate amount
+    const amount = Number(tour.costFrom) * Number(payload?.guestCount!);
+
+    const booking = await Booking.create(
+      [{
+        user: userId,
+        status: BOOKING_STATUS.PENDING,
+        ...payload,
+      }],
+      { session: session }
     );
+
+
+    const payment = await Payment.create(
+      [{
+        booking: booking[0]._id,
+        status: PAYMENT_STATUS.UNPAID,
+        transactionId: transactionId,
+        amount: amount,
+      }],
+      { session: session }
+    );
+
+    const updatedBooking = await Booking.findByIdAndUpdate(
+      booking[0]._id,
+      { payment: payment[0]._id },
+      { new: true, runValidators: true, session: session }
+    )
+      .populate("user", "name email phone address")
+      .populate("tour", "title costFrom")
+      .populate("payment");
+
+    // transaction rollback commit
+    await session.commitTransaction(); // transaction
+    session.endSession();
+
+    return updatedBooking;
+
+  } catch (error) {
+
+    await session.abortTransaction(); // transaction rollback
+    session.endSession();
+    throw error;
   }
-
-  // tour
-  const tour = await Tour.findById(payload.tour).select("costFrom");
-
-  if (!tour?.costFrom) {
-    throw new AppError(StatusCodes.BAD_REQUEST, "No tour cost found!");
-  }
-
-  // calculate amount
-  const amount = Number(tour.costFrom) * Number(payload?.guestCount!);
-
-  const booking = await Booking.create({
-    user: userId,
-    status: BOOKING_STATUS.PENDING,
-    ...payload,
-  });
-
-  const payment = await Payment.create({
-    booking: booking._id,
-    status: PAYMENT_STATUS.UNPAID,
-    transactionId: transactionId,
-    amount: amount,
-  });
-
-  const updatedBooking = await Booking.findByIdAndUpdate(
-    booking._id,
-    { payment: payment._id },
-    { new: true, runValidators: true }
-  ).populate("user", "name email phone address")
-   .populate("tour", "title costFrom")
-   .populate("payment",);
-
-  return updatedBooking;
 };
 
 // Frontend(localhost:5173) - User - Tour - Booking (Pending) - Payment(Unpaid) -> SSLCommerz Page -> Payment Complete -> Backend(localhost:5000/api/v1/payment/success) -> Update Payment(PAID) & Booking(CONFIRM) -> redirect to frontend -> Frontend(localhost:5173/payment/success)
